4ce2644d1891da5b5e86576146a76e9ce7b46ced
Jake <jakemacnaughton@gmail.com>
2018-06-11 16:12:00 -0400

    removed solution

--- queries.py
+++ queries.py
@@ -7,40 +7,25 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 def return_apple():
-    apple = session.query(Company).filter(Company.company=='Apple').first()
-    return apple
+    pass
 
 def return_disneys_industry():
-    disney = session.query(Company).filter(Company.company=='Walt Disney').first()
-    return disney.industry
+    pass
 
 def return_list_of_company_objects_ordered_alphabetically_by_symbol():
-    companies = session.query(Company).order_by(Company.symbol).all()
-    return companies
+    pass
 
 def return_list_of_dicts_of_tech_company_names_and_their_EVs_ordered_by_EV_descending():
-    tech = session.query(Company).filter_by(industry='Technology').order_by(Company.enterprise_value.desc())
-    arr = []
-    for co in tech:
-        obj = {'company': co.company, 'EV': co.enterprise_value}
-        arr.append(obj)
-    return arr
+    pass
 
 def return_list_of_consumer_products_companies_with_EV_above_225():
-    arr = []
-    for company in session.query(Company).filter(Company.industry=='Consumer products', Company.enterprise_value>225).all():
-        arr.append({'name': company.company})
-    return arr
+    pass
 
 def return_conglomerates_and_pharmaceutical_companies():
-    from sqlalchemy import or_
-    arr = []
-    for company in session.query(Company).filter(or_(Company.industry=='Conglomerate', Company.industry=='Pharmaceuticals')):
-        arr.append(company.company)
-    return arr
+    pass
 
 def avg_EV_of_dow_companies():
-    return session.query(func.avg(Company.enterprise_value)).first()
+    pass
 
 def return_industry_and_its_total_EV():
-    return session.query(Company.industry, func.sum(Company.enterprise_value)).group_by(Company.industry).all()
+    pass
